14427c906e0999db1db4c17334fbf2b7ac739e2f
Amirreza Azadi <a.azadi2001@gmail.com>
2023-04-10 06:18:19 +0430

    tailwind css added to the project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html","./src/style.css","./src/app.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
